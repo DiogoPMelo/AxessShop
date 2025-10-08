32a83ec3e9628018e9819f9d757f2c04b5edc1ca
DiogoPMelo <diogopmelo@icloud.com>
2025-10-08 11:39:16 +0100

    Initial Commit

---
+++ AxessShop/AxessShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxessShopApp.swift
+//  AxessShop
+//
+//  Created by Diogo Melo on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AxessShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AxessShopTests/AxessShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  AxessShopTests.swift
+//  AxessShopTests
+//
+//  Created by Diogo Melo on 8/10/25.
+//
+
+import Testing
+@testable import AxessShop
+
+struct AxessShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
